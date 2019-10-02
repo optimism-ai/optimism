84a0b715d66d3767f736219dbebd556c5fb0df3f
Oscar Olazabal <oscarolazabal.dev@gmail.com>
2019-10-02 11:28:48 -0700

    update comp

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,6 +2,8 @@
 import React, { useState } from "react";
 import { useAuth0 } from "../react-auth0-wrapper";
 import "./dashboard.css";
+import CanvasJSReact from './canvasjs.react.js';
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 const Dashboard = () => {
     const [showResult, setShowResult] = useState(false);
@@ -35,34 +37,72 @@ const Dashboard = () => {
 
     callApi()
 
+    const options = {
+      axisX: {
+        gridThickness: 0,
+        tickThickness: 0,
+        lineThickness: 0,
+      },
+      axisY: {
+        gridThickness: 0,
+        tickThickness: 0,
+        lineThickness: 0,
+        labelFontSize: 0,
+      },
+      data: [{
+        type: "bar",
+        dataPoints: [
+          { y: 5, label: "Work"},
+          { y: 7, label: "Learning"},
+          { y: 2, label: "Health"},
+          { y: 8, label: "Social"},
+          { y: 10, label: "Media"},
+          { y: 6, label: "Mental"},
+        ]
+      }]
+
     return (
       <div>
       {isAuthenticated && (
+        <div className="userInfo">
+          <div className="usrPic">
+            <h2>User Pic</h2>
+          </div>
+          <div class="usrName">
+            <h2>John Doe</h2>
+          </div>
+          <div className="doSurvey">
+            <Button variant="outline-primary">Do Survey</Button>
+          </div>
+        </div>
         <div className="container">
-            <div className="lhs">
-                <div className="sidebar">
-                    <h3>Username Picture</h3>
-                    <h4>Username</h4>
-                    <p>Do Daily Survey</p>
-                </div>
-                <div className="aspectScoreTb">
-                    <ul>
-                        <li>Aspect 1</li>
-                        <li>Aspect 2</li>
-                        <li>Aspect 3</li>
-                        <li>Aspect 4</li>
-                    </ul>
-                </div>
+          <div className="aspectInfo">
+            <CanvasJSChart options = {options} />
+          </div>
+          <div className="moodInfo">
+            <div className="moodModule">
+              <h5 className='moodHeading'>Mood 1</h5>
+              <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+                 totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta
+                 sunt explicabo. Nemo enim ipsam voluptatem</p>
             </div>
-            <div className="rhs">
-                <h2>Recent Moods</h2>
-                <ul>
-                    <li>Mood1</li>
-                    <li>Mood2</li>
-                    <li>Mood3</li>
-                    <li>Mood4</li>
-                </ul>
+            <div className="moodModule">
+              <h5 className='moodHeading'>Mood 2</h5>
+              <p>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+                totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta
+                sunt explicabo. Nemo enim ipsam voluptatem
+              </p>
             </div>
+            <div className="moodModule">
+              <h5 className='moodHeading'>Mood 3</h5>
+              <p>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+                totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta
+                sunt explicabo. Nemo enim ipsam voluptatem
+              </p>
+            </div>
+          </div>
         </div>
       )}
         {showResult && <code>{JSON.stringify(apiMessage, null, 2)}</code>}
