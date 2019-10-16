e72d99f61ab1fb6a32ab7e84d0b262ae09524ab9
Julian Coronado <julian.c28@gmail.com>
2019-10-16 10:39:58 -0700

    Homepage changes for a cleaner look

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,32 +5,26 @@ import HowArt from "./how.png";
 
 const Home = () => {
     return (
-      <div className="Home">
-
-        <img className="banner" src={WebLogo} alt="Optimism Logo"></img>
-
-        <div className="lander">
+      <div className="homepage">
 
           <div className="description">
-
-            <h1 className="quote">Be Happy, Stay Happy</h1>
+          <img className="banner" src={WebLogo} alt="Optimism Logo"/>
             <p>
-              Optimism will help you log your daily mood and activities. We intend to improve and maintain
-              your happiness. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-              ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-              in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-              non proident, sunt in culpa qui officia
+              Optimism is a daily mood tracker.
+            </p>
+            <p>
+            We intend to improve, track, and
+            </p>
+            <p>
+            maintain your overall happiness.
             </p>
           </div>
 
-          <div className="demo">
-            <h2 className="howTitle">
-              How It Works!
+          <div className="how">
+            <h2>
+              How does it work?
             </h2>
-            <div className="demo_pic">
-              <img className="howClipArt" src={HowArt} alt="How it Works"></img>
-            </div>
+            <img className="howClipArt" src={HowArt} alt="How it Works"/>
             <p className="howBody">
             The apps works by receiving input from the user which includes two things, their mood and what activities
             they have been up to. Once the user inputs their mood, our smart algorithms will give feedback and suggestions
@@ -42,7 +36,6 @@ const Home = () => {
             mood, we will never share your data. It is all encrypted with the SHA-256 protocol etc.
             </p>
           </div>
-        </div>
       </div>
    );
 }
